271e03f6a36dd952682f99a2df905a1ec5eca6eb
radityasurya <radityasurya@hotmail.com>
2020-09-08 00:30:34 +0200

    Fix eslint

--- server/src/api/services/account/account.model.js
+++ server/src/api/services/account/account.model.js
@@ -12,8 +12,8 @@ const AccountSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      // eslint-disable-next-line no-useless-escape
       match: [
+        // eslint-disable-next-line no-useless-escape
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         'Please enter a valid email',
       ],
