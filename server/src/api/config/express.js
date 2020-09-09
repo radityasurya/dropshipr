799ff486182a43877b4f071fd2c8c294bd5f71eb
radityasurya <radityasurya@hotmail.com>
2020-09-10 00:49:12 +0200

    Update express middlewares

--- server/src/api/config/express.js
+++ server/src/api/config/express.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const compress = require('compression');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 const cors = require('cors');
 
 const routes = require('../routes');
 const config = require('./config');
 
 const app = express();
+
+// parse body params and attach them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+
+// request logging. dev: console | production: file
+// app.use(morgan('combined'));
 
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
@@ -25,5 +42,13 @@ app.get('/', (req, res) =>
 );
 
 app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    status: err.status || 500,
+  });
+  next(err);
+});
 
 module.exports = app;
